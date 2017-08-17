a2840e3e278644c59195d79e1dc267dcb9efe886
tmyksj <33417830+tmyksj@users.noreply.github.com>
2017-08-17 21:12:50 +0900

    hadoop cluster, hbase, zookeeper

---
+++ docker/cluster/entrypoints/hadoop/hadoop-resource-manager.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service ssh restart
+
+${HADOOP_HOME}/bin/yarn --daemon start resourcemanager
+${HADOOP_HOME}/bin/yarn --daemon start proxyserver
+${HADOOP_HOME}/bin/mapred --daemon start historyserver
+
+stop_docker() {
+  ${HADOOP_HOME}/bin/yarn --daemon stop resourcemanager
+  ${HADOOP_HOME}/bin/yarn --daemon stop proxyserver
+  ${HADOOP_HOME}/bin/mapred --daemon stop historyserver
+  exit 0
+}
+
+trap "stop_docker" HUP INT QUIT TERM
+while true; do sleep 1; done
