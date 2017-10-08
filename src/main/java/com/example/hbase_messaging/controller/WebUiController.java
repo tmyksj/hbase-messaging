a95f9a75b2f53f45b36b9c1fe9e444285db7766a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2017-10-08 22:19:36 +0900

    add webui

---
+++ src/main/java/com/example/hbase_messaging/controller/WebUiController.java
@@ -0,0 +1,14 @@
+package com.example.hbase_messaging.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WebUiController {
+    @RequestMapping(path = "/webui", method = RequestMethod.GET)
+    public String get() {
+        return "/webui/index";
+    }
+}
+
