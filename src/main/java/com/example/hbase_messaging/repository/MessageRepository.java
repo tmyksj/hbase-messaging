a1d9fa0710f29c7ef5cb48768d3e300eeb2be48b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2017-10-08 19:49:42 +0900

    add seeds to repository

--- src/main/java/com/example/hbase_messaging/repository/MessageRepository.java
+++ src/main/java/com/example/hbase_messaging/repository/MessageRepository.java
@@ -1,11 +1,28 @@
 package com.example.hbase_messaging.repository;
 
 import com.example.hbase_messaging.entity.MessageEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 public interface MessageRepository {
     List<MessageEntity> get(String userIdFrom, String userIdTo);
-
     MessageEntity post(String userIdFrom, String userIdTo, String message);
+    void seed(Seed seed);
+
+    @Data
+    @EqualsAndHashCode
+    class Seed {
+        private List<Message> messageList;
+        private int numberOfMessages;
+
+        @Data
+        @EqualsAndHashCode
+        public static class Message {
+            private String userIdFrom;
+            private String userIdTo;
+            private String message;
+        }
+    }
 }
