418821dd5b56b0d7e1bf9c6a8a3604e07438995e
camilla key <maiarbts@gmail.com>
2017-11-11 10:15:48 +0900

    update seeds

--- src/main/java/com/example/hbase_messaging/repository/MessageRepository.java
+++ src/main/java/com/example/hbase_messaging/repository/MessageRepository.java
@@ -1,19 +1,11 @@
 package com.example.hbase_messaging.repository;
 
 import com.example.hbase_messaging.entity.MessageEntity;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 public interface MessageRepository {
     List<MessageEntity> get(String userIdFrom, String userIdTo);
     MessageEntity post(String userIdFrom, String userIdTo, String message);
-    void seed(Seed seed);
-
-    @Data
-    @EqualsAndHashCode
-    class Seed {
-        private int numberOfMessages;
-    }
+    void seed(int numberOfMessages);
 }
